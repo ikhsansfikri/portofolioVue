f5b2e5ce29afc3d6abbd70584ea25eb7d96f33be
ikhsansfikri <m_ikhsan20@mhs.usk.ac.id>
2026-07-27 15:12:18 +0700

    Refactor code structure for improved readability and maintainability

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -3,6 +3,9 @@ import cbt  from "@/assets/projects/cbt.png";
 import mitraPema from "@/assets/projects/mitra-pema.png";
 import umkmPema from "@/assets/projects/umkm-pema.png";
 import indatu from "@/assets/projects/indatu.png";
+import promise from "@/assets/projects/promise.png";
+import esign from "@/assets/projects/esign.png";
+import simapres from "@/assets/projects/simapres.png";
 
 const portfolioData = {
   name: "M. Ikhsan Fikri",
@@ -14,35 +17,66 @@ const portfolioData = {
   projects: [
     {
       id: 1,
+      title: "Electronic Signature",
+      image: esign,
+      description: "Rebuilding the local Electronic Signature application for procurement needs within Universitas Syiah Kuala, transforming the previously fragmented and unstable system into a single, reliable centralized system integrated with the university's procurement workflow.",
+      technologies: ["Laravel","Tailwind CSS","Python", "MySQL", "Docker", "Caddy"],
+      link: "https://esign.usk.ac.id",
+      year: 2026
+    },
+    {
+      id: 2,
+      title: "Promise",
+      image: promise,
+      description: "This is a landing page apps was built to provide easier access to various procurement applications within Universitas Syiah Kuala.",
+      technologies: ["Next.js","Tailwind CSS", "TypeScript", "Docker", "Caddy"],
+      link: "https://promise.usk.ac.id",
+      year: 2026
+    },
+    {
+      id: 3,
+      title: "SIMAPRES",
+      image: simapres,
+      description: "Developing the SIMAPRES application for the Student Affairs and Achievement Directorate of Universitas Syiah Kuala to digitize and optimize the management and selection of scholarship admissions within Universitas Syiah Kuala.",
+      technologies: ["Django", "React", "Bootstrap", "MySQL"],
+      link: "https://simapres.usk.ac.id",
+      year: 2025
+    },
+    {
+      id: 4,
       title: "Computer Based Testing (CBT)",
       image: cbt,
       description: "A fully-featured Computer Based Test website with features for creating, managing, and administering online exams.",
       technologies: ["Django", "React", "Bootstrap", "MySQL"],
-      link: "https://cbt.ikhsanfikri.xyz"
+      link: "https://cbt.ikhsanfikri.xyz",
+      year: 2024
     },
     {
-      id: 2,
+      id: 5,
       title: "Vendor Management System",
       image: mitraPema,
       description: "A simple and intuitive vendor management system to help users stay organized and productive.",
       technologies: ["Laravel", "React", "Tailwind CSS", "MySQL"],
-      link: "https://mitra.ptpema.co.id"
+      link: "https://mitra.ptpema.co.id",
+      year: 2023
     },
     {
-      id: 3,
+      id: 6,
       title: "UMKM Infomation System",
       image: umkmPema,
       description: "A system designed to help small and medium-sized enterprises (SMEs) streamline and optimize their business operations.",
       technologies: ["Laravel", "React", "Tailwind CSS", "MySQL"],
-      link: "https://umkm.ptpema.co.id"
+      link: "https://umkm.ptpema.co.id",
+      year: 2023
     },
     {
-      id: 4,
+      id: 7,
       title: "Integrated Data Management System",
       image: indatu,
       description: "An Integrated Data Integration Website designed to centralize and streamline data management across multiple systems.",
       technologies: ["CodeIgniter", "Tailwind CSS", "MySQL"],
-      link: "https://indatu.unsam.ac.id"
+      link: "https://data.unsam.ac.id",
+      year: 2022
     }
   ],
   experiences: [
--- src/types/index.ts
+++ src/types/index.ts
@@ -5,6 +5,7 @@ export interface Project {
   description: string;
   technologies: string[];
   link: string;
+  year?: number;
 }
 
 export interface Experience {
